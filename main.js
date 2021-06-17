94e4732f5fdbd18eaca8b036ff3dbe481975b0c4
amv1017 <amv1017@mail.ru>
2021-06-18 00:14:04 +0300

    change texture onClick

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-// Brad Traversy - Getting Started With Three.JS
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+let renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+let geometry = new THREE.BoxGeometry(2, 2, 2)
+let textureIndex = 0
+let textureURL = [
+    'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQ36Li_NGgeQEnrU6pj7G0S5OkxPVfNjl35f4JOqBQK7N_TnoxkWEmkVLqlZS606fmjM&usqp=CAU',
+    'https://www.the3rdsequence.com/texturedb/download/13/texture/jpg/256/dark+parquet-256x256.jpg',
+]
+let loader = new THREE.TextureLoader()
+let material = new THREE.MeshBasicMaterial()
+let cube = new THREE.Mesh(geometry, material)
 
-let scene, camera, renderer, cube;
+camera.position.z = 5
 
-function init() {
-    scene = new THREE.Scene()
-    camera = new THREE.PerspectiveCamera(75,(window.innerWidth / window.innerHeight),0.1,1000)
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-    const geometry = new THREE.BoxGeometry(2, 2, 2)
-
-    let textureurls = [
-      'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQ36Li_NGgeQEnrU6pj7G0S5OkxPVfNjl35f4JOqBQK7N_TnoxkWEmkVLqlZS606fmjM&usqp=CAU',
-      'https://www.the3rdsequence.com/texturedb/download/13/texture/jpg/256/dark+parquet-256x256.jpg',
-      'https://thumbs.dreamstime.com/b/square-background-light-blue-abstract-postcard-83587745.jpg',
-      'https://5.imimg.com/data5/CN/NH/MY-2/wallpaper1-jpg-500x500.jpg'
-    ]
-    const textureurl = textureurls[0]
-    const texture = new THREE.TextureLoader().load(textureurl)
-    // const material = new THREE.MeshBasicMaterial({color: 0x0000ff})
-    const material = new THREE.MeshBasicMaterial({map: texture})
-    cube = new THREE.Mesh(geometry, material);
+loader.load(textureURL[textureIndex], (texture) => {
+    material.map = texture
+    textureIndex++
     scene.add(cube)
-    camera.position.z = 5
-}
+})
 
 function animate() {
     requestAnimationFrame(animate)
     cube.rotation.x += 0.01
     cube.rotation.y += 0.01
     cube.rotation.z += 0.01
-    renderer.render(scene,camera)
+    renderer.render(scene, camera)
 }
 
 function onWindowResize() {
@@ -42,5 +38,14 @@ function onWindowResize() {
 
 window.addEventListener('resize',onWindowResize,false)
 
-init()
+window.addEventListener('click', () => {
+    loader.load(textureURL[textureIndex], (texture) => {
+        material.map = texture
+        textureIndex++
+        if (textureIndex > textureURL.length-1) {
+            textureIndex = 0
+        }
+    })
+})
+
 animate()
